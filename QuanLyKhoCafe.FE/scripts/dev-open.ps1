6ddf1ef6341caf56db4b5fe26639cae1c2fc1ee2
TUVY <TUVY@TIUV>
2026-06-10 11:33:07 +0700

    Open browser externally and restore action accents

---
+++ QuanLyKhoCafe.FE/scripts/dev-open.ps1
@@ -0,0 +1,16 @@
+$port = 5173
+
+while (Get-NetTCPConnection -LocalPort $port -State Listen -ErrorAction SilentlyContinue) {
+  $port++
+}
+
+$url = "http://localhost:$port/"
+
+Start-Job -ScriptBlock {
+  param($targetUrl)
+
+  Start-Sleep -Seconds 2
+  Start-Process $targetUrl
+} -ArgumentList $url | Out-Null
+
+npx vite --host 127.0.0.1 --port $port --strictPort
